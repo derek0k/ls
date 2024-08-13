d51ccb666891f6fdfed2c1fec3679f0dddb0e677
derek0k <derek0kim@gmail.com>
2024-08-13 21:13:33 +0900

    feat: 국가 메달 관리 기능 구현
    - 상태 관리와 메달 집계 추가 기능 구현
    - 메달 집계 리스트 출력하기
    - 메달 수 업데이트하기
    - 메달 수 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,190 @@
+import { useState } from 'react';
 import './App.css';
 
-export default function App() {
+const INITIAL_COUNTRY_FORM = { id: 0, country: '', gold: 0, silver: 0, bronze: 0 };
+
+const COUNTRY_FORM_INPUTS = [
+  {
+    label: '국가',
+    name: 'country',
+    type: 'text',
+  },
+  {
+    label: '금메달',
+    name: 'gold',
+    type: 'number',
+  },
+  {
+    label: '은메달',
+    name: 'silver',
+    type: 'number',
+  },
+  {
+    label: '동메달',
+    name: 'bronze',
+    type: 'number',
+  },
+];
+
+const validateMedalCount = (count, medalType) => {
+  if (isNaN(parseInt(count))) {
+    return {
+      isValid: false,
+      message: `${medalType} 수는 숫자여야 합니다`,
+    };
+  }
+
+  if (parseInt(count) < 0) {
+    return {
+      isValid: false,
+      message: `${medalType} 수를 확인해주세요`,
+    };
+  }
+
+  return { isValid: true };
+};
+
+const validateCountryForm = countryForm => {
+  if (countryForm.country === '') {
+    return {
+      isValid: false,
+      message: '국가를 입력해주세요',
+    };
+  }
+
+  const goldValidation = validateMedalCount(countryForm.gold, '금메달');
+  if (!goldValidation.isValid) return goldValidation;
+
+  const silverValidation = validateMedalCount(countryForm.silver, '은메달');
+  if (!silverValidation.isValid) return silverValidation;
+
+  const bronzeValidation = validateMedalCount(countryForm.bronze, '동메달');
+  if (!bronzeValidation.isValid) return bronzeValidation;
+
+  return { isValid: true, message: '성공' };
+};
+
+const CountryRankListItem = ({ item, onDeleteClick }) => {
   return (
-    <div className="container">
-      <h1>2024 파리 올림픽</h1>
-      <form>
-        <div>
-          <label htmlFor="country">국가명</label>
-          <input type="text" id="country" name="country" placeholder="국가 입력" />
-        </div>
-
-        <div>
-          <label htmlFor="goldMedals">금메달</label>
-          <input type="number" id="goldMedals" name="goldMedals" placeholder="0" />
-        </div>
-
-        <div>
-          <label htmlFor="silverMedals">은메달</label>
-          <input type="number" id="silverMedals" name="silverMedals" placeholder="0" />
-        </div>
-
-        <div>
-          <label htmlFor="bronzeMedals">동메달</label>
-          <input type="number" id="bronzeMedals" name="bronzeMedals" placeholder="0" />
-        </div>
-
-        <div className="buttonBox">
-          <button type="submit">국가 추가</button>
-          <button type="submit">업데이트</button>
-        </div>
-      </form>
-
-      <table>
-        <thead>
-          <tr>
-            <th>국가명</th>
-            <th>금메달</th>
-            <th>은메달</th>
-            <th>동메달</th>
-            <th>액션</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td>Brazil</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-            <td>
-              <button>삭제</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Brazil</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-            <td>
-              <button>삭제</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Brazil</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-            <td>
-              <button>삭제</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <div>
+      <div>{item.country}</div>
+      <div>{item.gold}</div>
+      <div>{item.silver}</div>
+      <div>{item.bronze}</div>
+      <button onClick={onDeleteClick}>삭제</button>
     </div>
   );
+};
+
+export const CountryRankList = ({ countryList, onDeleteClick }) => {
+  const sortedCountryList = countryList.toSorted((a, b) => {
+    return b.gold - a.gold;
+  });
+  return (
+    <>
+      {sortedCountryList.map(item => {
+        return (
+          <CountryRankListItem
+            key={item.country}
+            item={item}
+            onDeleteClick={() => onDeleteClick(item.country)}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+const CountryFormItem = ({ inputItem, onChange, value }) => {
+  return (
+    <div>
+      <label>{inputItem.label}</label>
+      <input type={inputItem.type} name={inputItem.name} onChange={onChange} value={value} />
+    </div>
+  );
+};
+
+export const CountryForm = ({ countryForm, onChange, onAddClick, onUpdateClick }) => {
+  return (
+    <>
+      {COUNTRY_FORM_INPUTS.map(inputItem => {
+        return (
+          <CountryFormItem
+            key={inputItem.name}
+            inputItem={inputItem}
+            onChange={onChange}
+            value={countryForm[inputItem.name]}
+          />
+        );
+      })}
+      <button onClick={onAddClick}>국가 추가</button>
+      <button onClick={onUpdateClick}>업데이트</button>
+    </>
+  );
+};
+
+export const CountrySection = () => {
+  const [countryForm, setCountryForm] = useState(INITIAL_COUNTRY_FORM);
+  const [countryList, setCountryList] = useState([]);
+
+  const resetCountryForm = () => {
+    setCountryForm(INITIAL_COUNTRY_FORM);
+  };
+
+  const handleFormChange = e => {
+    const { name, value } = e.target;
+    const changedValue = ['gold', 'silver', 'bronze'].includes(name) ? Number(value) : value;
+
+    setCountryForm(prev => ({ ...prev, [name]: changedValue }));
+  };
+
+  const handleAddCountry = () => {
+    const isExist = countryList.some(item => item.country === countryForm.country);
+    if (isExist) return alert('이미 존재하는 국가입니다');
+
+    const { isValid, message } = validateCountryForm(countryForm);
+    if (!isValid) return alert(message);
+
+    setCountryList(prev => [...prev, countryForm]);
+    resetCountryForm();
+  };
+
+  const handleUpdateCountry = () => {
+    const isExist = countryList.some(item => item.country === countryForm.country);
+    if (!isExist) return alert('존재하지 않는 국가입니다');
+
+    setCountryList(prev =>
+      prev.map(item => {
+        if (item.country === countryForm.country) return countryForm;
+        return item;
+      }),
+    );
+  };
+
+  const handleDeleteCountry = country => {
+    setCountryList(prev => prev.filter(item => item.country !== country));
+  };
+
+  return (
+    <>
+      <CountryForm
+        countryForm={countryForm}
+        onChange={handleFormChange}
+        onAddClick={handleAddCountry}
+        onUpdateClick={handleUpdateCountry}
+      />
+      <CountryRankList countryList={countryList} onDeleteClick={handleDeleteCountry} />
+    </>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <h1>2024 파리 올림픽</h1>
+      <CountrySection />
+    </>
+  );
 }
+
+export default App;
