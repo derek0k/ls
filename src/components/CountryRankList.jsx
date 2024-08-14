8c1f19027c25f0776ce6a673ca5443fbce87a12a
derek0k <derek0kim@gmail.com>
2024-08-14 18:55:06 +0900

    feat: 국가 목록의 존재 여부 확인에 따른 조건부 렌더링 추가

--- src/components/CountryRankList.jsx
+++ src/components/CountryRankList.jsx
@@ -18,6 +18,9 @@ export const CountryRankList = ({ countryList, onDeleteClick }) => {
   const sortedCountryList = countryList.toSorted((a, b) => {
     return b.gold - a.gold;
   });
+  if (Array.isArray(countryList) && countryList.length === 0)
+    return <p className="no-countries-message">아직 추가된 국가가 없습니다. 메달을 추적하세요!</p>;
+
   return (
     <table>
       <thead>
